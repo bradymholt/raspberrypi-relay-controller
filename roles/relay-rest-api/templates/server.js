dbb7fe981c68a80ad2cd6761ae61b30926d4583c
Brady Holt <brady.holt@gmail.com>
2016-12-15 15:09:25 -0600

    Broadcast UDP datagram for IP discovery

--- roles/relay-rest-api/templates/server.js
+++ roles/relay-rest-api/templates/server.js
@@ -20,20 +20,39 @@ server.route({
         let relay = request.payload.relay;
         if (relay.state === "closed") {
             let relayPin = relayToGpioPinMapping[relayNumber];
-            let duration = Number(relay.close_duration || defaultPinOnDuration);        
+            let duration = Number(relay.close_duration || defaultPinOnDuration);
             console.log(JSON.stringify(relay));
-            toggleGpioPinOn(relayPin, duration);    
+            toggleGpioPinOn(relayPin, duration);
         }
-        
+
         reply().code(204);
     }
 });
 
+// Start hapi.js HTTP server
 server.start((err) => {
     if (err) {
         throw err;
     }
     console.log(`Server running at: ${server.info.uri}`);
+});
+
+// Broadcast API metadata to UDP socket for discovery by other devices
+const broadcastSocket = dgram.createSocket("udp4");
+const os = require('os');
+const broadcastPort = 41234;
+const broadcastMessage = "Relay API";
+const broadcastIntervalMilliseconds = 60000;
+const broadcastInterface = Object.keys(os.networkInterfaces()).filter((iface) => { return iface != 'lo'; })[0];
+const hostAddress = os.networkInterfaces()[broadcastInterface][0].address;
+// Replace last octet of hostAddress with .255 so 192.168.1.35 becomes 192.168.1.255
+const broadcastAddress = hostAddress.substr(0, hostAddress.lastIndexOf(".")) + ".255";
+broadcastSocket.bind(() => {
+    broadcastSocket.setBroadcast(true);
+    setInterval(function (socket) {
+        console.log(`Broadcasting API metadata to: ${broadcastAddress}:${broadcastPort}`);
+        socket.send(broadcastMessage, 0, broadcastMessage.length, broadcastPort, broadcastAddress);
+    }, broadcastIntervalMilliseconds, broadcastSocket);
 });
 
 function toggleGpioPinOn(pin, durationMilliseconds) {
